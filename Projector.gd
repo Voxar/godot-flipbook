efa8c4c1d3f17d9b3dca80a44e73bc5b18cd3e09
Patrik Sjöberg <voxxar@gmail.com>
2023-04-09 23:17:52 +0200

    Nice

--- Projector.gd
+++ Projector.gd
@@ -1,10 +1,38 @@
 extends SpotLight3D
 
+var timer = Timer.new()
 
-#func _ready():
-	#var vpt = ($"../Viewport" as SubViewport).get_texture();
-	#light_projector = vpt;
+func _ready():
+	timer.connect("timeout", self.timeout)
+	timer.wait_time = 0.1;
+	timer.one_shot = false
+	add_child(timer)
+	timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(_delta):
-#	pass
+var time = 0
+var tt = 0
+func _process(delta):
+	time += delta
+	if (time < 3): 
+		self.light_energy = 0
+	elif time < 3.1:
+		self.light_energy = 5
+	elif time < 3.4:
+		self.light_energy = 0
+	elif time < 4.3:
+		if (tt == 0 or time > tt): 
+			self.light_energy = randf_range(1, 10)
+			tt = time + randf_range(0.02, 0.1)
+	elif time < 5:
+		self.light_energy = 20
+	
+func timeout():
+	var vpt = ($"../Viewport" as SubViewport)
+	vpt.set_process(true)
+	vpt.set_update_mode(SubViewport.UPDATE_ALWAYS)
+	var tex = vpt.get_texture();
+	self.set_process(true)
+	self.light_projector = tex;
+	var b = $"../World/Node/Node3D/CSGBox3D4"
+	
+
