9796d5f1c056bcdcd86ebb8a002c8ff025154543
Patrik Sjöberg <voxxar@gmail.com>
2023-04-09 21:26:52 +0200

    initial

---
+++ Projector.gd
@@ -0,0 +1,10 @@
+extends SpotLight3D
+
+
+#func _ready():
+	#var vpt = ($"../Viewport" as SubViewport).get_texture();
+	#light_projector = vpt;
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(_delta):
+#	pass
